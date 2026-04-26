30d6e66fe3d82783f9ad70d271e0041954fdcdbc
Pawan1718 <pmohokar22@gmail.com>
2026-04-27 00:55:16 +0530

    UI UX v2 upgrade

---
+++ Shala.Web/Components/Common/AppSectionSidebarItem.cs
@@ -0,0 +1,15 @@
+﻿using MudBlazor;
+
+namespace Shala.Web.Components.Common;
+
+public sealed class AppSectionSidebarItem
+{
+    public string Key { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
+    public string Icon { get; set; } = Icons.Material.Filled.Settings;
+
+    public string? Badge { get; set; }
+    public Color BadgeColor { get; set; } = Color.Primary;
+
+    public bool Disabled { get; set; }
+}
